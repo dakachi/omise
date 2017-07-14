b0a5040400a0dac876ac101d80a710da4beef8fc
dakachi <david87dang@gmail.com>
2017-07-14 11:39:41 +0700

    add file to repo

---
+++ omise.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Plugin Name: Membership Omise Gateway
+ * Plugin URI:  https://github.com/dakachi/omise
+ * Version:     1.0
+ * Author:      Dakachi
+ *
+ * @package Membership2
+ */
+
+require_once dirname(__FILE__).'/vendor/autoload.php';
+
+define('OMISE_PUBLIC_KEY', 'pkey_test_58lpgvlp7uulsxiyf72');
+define('OMISE_SECRET_KEY', 'skey_test_58lpgvlprrfxthnku0v');
